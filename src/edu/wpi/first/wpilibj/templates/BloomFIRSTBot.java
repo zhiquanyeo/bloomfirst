82f8b78207ec2424d9bd656386e1dc2a20e4ef20
Zhiquan Yeo <zhiquan.yeo@gmail.com>
2014-12-13 12:22:43 -0500

    [REVERT] Updated code for Sensor Demo

--- src/edu/wpi/first/wpilibj/templates/BloomFIRSTBot.java
+++ src/edu/wpi/first/wpilibj/templates/BloomFIRSTBot.java
@@ -8,15 +8,10 @@
 package edu.wpi.first.wpilibj.templates;
 
 
-import edu.wpi.first.wpilibj.ADXL345_I2C;
-import edu.wpi.first.wpilibj.AnalogChannel;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,8 +21,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class BloomFIRSTBot extends SimpleRobot {
-    //Some constants
-    private final double RANGEFINDER_SCALE_FACTOR = 0.977; 
     
     /*==== Robot Components ====
     What parts the robot has
@@ -38,20 +31,10 @@ public class BloomFIRSTBot extends SimpleRobot {
     */
     RobotDrive drivetrain = new RobotDrive(4, 3);
     
-    //Rangefinder
-    private AnalogChannel rangefinder = new AnalogChannel(7);
+    private DigitalInput frontSwitch = new DigitalInput(1);
+    private DigitalInput backSwitch = new DigitalInput(2);
     
-    //Simple limit switch
-    private DigitalInput limitSwitch = new DigitalInput(1);
     
-    //Accelerometer
-    private ADXL345_I2C accelerometer = new ADXL345_I2C(1, ADXL345_I2C.DataFormat_Range.k2G);
-    
-    /*==== Human Input Components ====
-    
-    */
-    Joystick leftJoystick = new Joystick(1);
-    Joystick rightJoystick = new Joystick(2);
     
     /**
      * This function is called once each time the robot enters autonomous mode.
@@ -61,24 +44,19 @@ public class BloomFIRSTBot extends SimpleRobot {
         //Turn safety off since we won't be feeding the watchdog
         drivetrain.setSafetyEnabled(false);
         
-        //Let's set the robot driving forward for a little bit (at a safe speed!)
-        drivetrain.drive(0.2, 0);
+        double speed = 0.2;
         
         while (isEnabled() && isAutonomous()) {
-            //drive forward until we are 50cm (500mm) away from a target then backup and spin around
-            double range = getRangefinderDistance();
-            SmartDashboard.putNumber("Range", range);
-            SmartDashboard.putBoolean("Limit Switch", limitSwitch.get());
-            if (range < 500) {
-                //Backup for 2 seconds
-                drivetrain.drive(-0.1, 0);
-                Timer.delay(2.0);
-                //Turn for 5 seconds
-                drivetrain.drive(0, 0.2);
-                Timer.delay(5.0);
-                //Reset the driving speed
-                drivetrain.drive(0.2, 0);
+            //If we are driving forward AND the front switch is pressed
+            //drive backwards
+            if (speed > 0 && !frontSwitch.get()) {
+                speed = -0.2; //Set reverse
             }
+            else if (speed < 0 && !backSwitch.get()) {
+                speed = 0.2;
+            }
+            
+            drivetrain.drive(speed, 0);
         }
     }
 
@@ -86,18 +64,7 @@ public class BloomFIRSTBot extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-        drivetrain.setSafetyEnabled(true);
-        double accelX, accelY, accelZ;
-        while (isEnabled() && isOperatorControl()) {
-            //1) Sense
-            SmartDashboard.putNumber("Range", getRangefinderDistance());
-            SmartDashboard.putBoolean("Limit Switch", limitSwitch.get());
-            accelX = accelerometer.getAcceleration(ADXL345_I2C.Axes.kX);
-            accelY = accelerometer.getAcceleration(ADXL345_I2C.Axes.kY);
-            accelZ = accelerometer.getAcceleration(ADXL345_I2C.Axes.kZ);
-            String accelStr = accelX + ":" + accelY + ":" + accelZ;
-            SmartDashboard.putString("Acceleration (x:y:z)", accelStr);
-        }
+        
     }
     
     /**
@@ -107,10 +74,4 @@ public class BloomFIRSTBot extends SimpleRobot {
     
     }
     
-    /***** Helper Functions ******/
-    private double getRangefinderDistance() {
-        //scaling factor is ~0.977mV/mm
-        //multiply voltage by 1000 to get mV, then divide by scale factor
-        return rangefinder.getVoltage() * 1000 / RANGEFINDER_SCALE_FACTOR;
-    }
 }
